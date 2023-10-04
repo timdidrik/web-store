1e509415f13e4ac77223a58ac9b760c86637853d
timdidrik <tdsandma@hiof.no>
2023-10-04 16:45:38 +0200

    Added functions to create random products.

--- src/features/createProduct.ts
+++ src/features/createProduct.ts
@@ -1,5 +1,6 @@
-import type { Product} from "./types";
+import type { Faker, Product } from "./types"
 
+// Define some fake products
 const fakeTitles: string[] = [
     "Roadhog Statue",
     "Half-Life 3",
@@ -8,6 +9,7 @@ const fakeTitles: string[] = [
     "Carbon Shoes",
 ]
 
+// Define some fake categories
 const fakeCategories: string[] = [
     "Statues",
     "Video Games",
@@ -15,7 +17,36 @@ const fakeCategories: string[] = [
     "Clothing",
 ]
 
+// Define some fake prices
 const fakePrices: number[] = [
     1499, 699, 25000, 150, 2499
 ]
 
+// Get a random item from the array
+const getRandomItem = <T>(items: T[]) => {
+    const randomIndex = Math.floor(Math.random() * items.length);
+
+    return items[randomIndex];
+}
+
+// Generate a fake product
+export const faker: Faker = {
+    // Gets a random X from fake titles/categories/prices
+    title: () => getRandomItem(fakeTitles),
+    category: () => getRandomItem(fakeCategories),
+    price: () => getRandomItem(fakePrices),
+}
+
+
+// Get as many fake random products as you want
+const getRandomProducts = (count: number): Product[] =>
+
+// Array.from takes two arguments, here --->
+// length = first argument, () = second argument which
+// is the arrow function containing faker objects
+// I think - brain meltdown, I will move on.
+  Array.from({ length: count }, () => ({
+    title: faker.title(),
+    category: faker.category(),
+    price: faker.price(),
+  }));
