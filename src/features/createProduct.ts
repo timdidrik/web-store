b7875540c5ebb8c58a98dbb0d3d41b477a7a7082
timdidrik <tdsandma@hiof.no>
2023-10-04 14:48:33 +0200

    Added some fake products.

---
+++ src/features/createProduct.ts
@@ -0,0 +1,21 @@
+import type { Product} from "./types";
+
+const fakeTitles: string[] = [
+    "Roadhog Statue",
+    "Half-Life 3",
+    "Overpriced RTX 4080",
+    "Battle Pass with nothing good in it",
+    "Carbon Shoes",
+]
+
+const fakeCategories: string[] = [
+    "Statues",
+    "Video Games",
+    "Electronics",
+    "Clothing",
+]
+
+const fakePrices: number[] = [
+    1499, 699, 25000, 150, 2499
+]
+
