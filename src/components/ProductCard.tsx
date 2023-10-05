ea1d9ca781f693e793d48a8a900031aa96442164
timdidrik <tdsandma@hiof.no>
2023-10-05 22:13:18 +0200

    Fixed prop syntax.

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,18 +1,19 @@
 'use client';
 
 import React from "react";
-import type { Product } from "@/features/types";
+import type { ProductCard } from "@/features/types";
 
-export default function ProductCard(props: Product) {
+export default function ProductCard(props: ProductCard) {
   const handleAddToCart = () => {
     // Implement logic to add the product to the cart
   };
 
+  // props.product
   return (
     <div className="product-card">
-      <h2>{props.title}</h2>
-      <p>Category: {props.category}</p>
-      <p>Price: {props.price} NOK</p>
+      <h2>{props.product.title}</h2>
+      <p>Category: {props.product.category}</p>
+      <p>Price: {props.product.price} NOK</p>
       <button className="button" onClick={handleAddToCart}>Add to Cart</button>
     </div>
   );
