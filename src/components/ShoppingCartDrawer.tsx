107114eb3c3b9db03d46a19980d4493b356bc5c7
timdidrik <tdsandma@hiof.no>
2023-10-12 15:24:47 +0200

    Fixed z-index issues for drawer and made it work in smaller window.

--- src/components/ShoppingCartDrawer.tsx
+++ src/components/ShoppingCartDrawer.tsx
@@ -4,37 +4,34 @@ import type { CartItemProps, ShoppingCartDrawerProps, ShoppingCartButtonProps } 
 import React from "react";
 
 export default function ShoppingCartDrawer({ isCartOpen, toggleCart, cart }: ShoppingCartDrawerProps & ShoppingCartButtonProps) {
-    const cartItems = [];
+  const closeCart = () => {
+    toggleCart();
+  }
 
-    const closeCart = () => {
-        toggleCart();
-    }
+  const addToCart = (product: CartItemProps) => {
+    // doSomething
+  }
 
-    const addToCart = (product: CartItemProps) => {
-        // doSomething
-    }
+  const removeFromCart = (product: CartItemProps) => {
+    // doSomething
+  }
 
-    const removeFromCart = (product: CartItemProps) => {
-        // doSomething
-    }
+  const purchaseCart = () => {
+    // doSomething
+  }
 
-    const purchaseCart = () => {
-        // doSomething
-    }
-
-    return (
-      <div className={`cart-drawer ${isCartOpen ? 'open' : ''}`}>
-        <button className="close-cart-button" onClick={closeCart}>Exit</button>
-        {cart.map((product) => (
-          <div key={product.title}> {/* Assuming title is a unique identifier */}
-            <h3>{product.title}</h3>
-            <input type="number" value={product.quantity} />
-            <button onClick={() => { addToCart(product) }}>+</button>
-            <button onClick={() => { removeFromCart(product) }}>-</button>
-          </div>
-        ))}
-        <button className="purchase-cart-button" onClick={purchaseCart}>Purchase</button>
-      </div>
-    );
-
+  return (
+    <div className={`cart-drawer ${isCartOpen ? 'open' : ''}`} >
+      <button className="close-cart-button" onClick={closeCart}>Exit</button>
+      {cart.map((product) => (
+        <div key={product.title}>
+          <h3>{product.title}</h3>
+          <input type="number" value={product.quantity} />
+          <button className="close-cart-button" onClick={() => { addToCart(product) }}>+</button>
+          <button className="close-cart-button" onClick={() => { removeFromCart(product) }}>-</button>
+        </div>
+      ))}
+      <button className="purchase-cart-button" onClick={purchaseCart}>Purchase</button>
+    </div>
+  );
 }
