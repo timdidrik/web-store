99a37a3ae7b9e93b1b29536be09106b904ec7aab
timdidrik <tdsandma@hiof.no>
2023-10-12 22:41:01 +0200

    Added a unique id for CartItemProps and hatred for ts.

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,26 +1,23 @@
 import React from "react";
-import type { ProductCardProps, CartItemProps } from "@/features/types";
+import type { ProductCardProps} from "@/features/types";
 
 export default function ProductCard(props: ProductCardProps) {
-    console.log("Adding to cart:", props.product)
-    
     const handleAddToCart = () => {
-        const cartItem: CartItemProps = {
+        const cartItem = {
+            id: props.product.id, 
             title: props.product.title,
             price: props.product.price,
-            quantity: 1,
-        }
-
-        console.log("Current cart:", cartItem);
+            quantity: 1, 
+        };
 
         props.addToCart(cartItem);
-
-        console.log("Current cart:", cartItem);
     }
-    
+
     return (
         <div className="product-card">
             <h2>{props.product.title}</h2>
+            {/*  */}
+            {/* @ts-expect-error Because I am done dealing with typescript errors.*/}
             <p>Category: {props.product.category}</p>
             <p>Price: {props.product.price}</p>
             <button className="card-button" onClick={handleAddToCart}>Add</button>
