898cc06d07b43f3b8401aa3addffe9c205fc9f81
timdidrik <tdsandma@hiof.no>
2023-10-13 20:25:52 +0200

    Was bugging out a little with a higher value so set it lower.

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { getRandomProducts } from "@/features/createProduct";
 
 export function GET() {
-    const products = getRandomProducts(5)
+    const products = getRandomProducts(2)
 
     return NextResponse.json(
         { data: Array.from(products)},
