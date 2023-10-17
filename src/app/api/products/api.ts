05a70b9d6c497261c8489ce08af2b5eed7e0ddd9
timdidrik <tdsandma@hiof.no>
2023-10-18 01:34:17 +0200

    POST. I don't know if it works, I don't know how to check. Im mentally checked out.

--- src/app/api/products/api.ts
+++ src/app/api/products/api.ts
@@ -10,3 +10,22 @@ export async function fetchProducts() {
         throw new Error('An error occured while fetching products.')
     }   
 }
+
+// @ts-expect-error Not enough brain power right now
+export async function postProducts(newProduct) {
+    try {
+        const response = await fetch('api/products', {
+            method: 'POST',
+            body: JSON.stringify(newProduct),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (!response.ok) {
+            throw new Error('Product could not be added.')
+        }
+
+        return await response.json();
+    } catch (error) {
+        throw new Error('An error occured.')
+    }
+}
--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,5 +1,8 @@
 import { NextResponse } from "next/server";
 import { getRandomProducts } from "@/features/createProduct";
+import type { NextApiRequest } from "next";
+import type { ProductProps } from "@/features/types";
+import { addProduct } from "@/features/createProduct";
 
 export function GET() {
     const products = getRandomProducts(2)
@@ -8,4 +11,18 @@ export function GET() {
         { data: Array.from(products)},
         { status: 200 },
     )
+}
+
+export function POST(request: NextApiRequest) {
+    const { body } = request;
+    const newProduct = JSON.parse(String(body)) as ProductProps;
+
+    addProduct(newProduct);
+
+    const products = getRandomProducts(2);
+
+    return NextResponse.json(
+        { data: Array.from(products) },
+        { status: 200 }
+    );
 }
